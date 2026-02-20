55a3ea71e5bec5ec4aa2ab48565c9fc036da095b
Gwynneth Uhrhammer <gleece@gmail.com>
2026-02-20 15:39:53 -0800

    Get rid of separate items list

--- js/categorizer.js
+++ js/categorizer.js
@@ -1,5 +1,6 @@
 /**
  * Categorize a grocery item by matching card name against store layout keywords.
+ * Keyword order in the array determines sort order within a section.
  *
  * @param {string} cardName
  * @param {Object} storeLayout
@@ -12,17 +13,9 @@ function categorize(cardName, storeLayout) {
   for (var si = 0; si < sections.length; si++) {
     var section = sections[si];
 
-    // Check items array first (more specific match)
-    for (var ii = 0; ii < section.items.length; ii++) {
-      if (name.includes(section.items[ii].toLowerCase())) {
-        return { sectionIndex: si, itemIndex: ii };
-      }
-    }
-
-    // Then check keywords
     for (var ki = 0; ki < section.keywords.length; ki++) {
       if (name.includes(section.keywords[ki].toLowerCase())) {
-        return { sectionIndex: si, itemIndex: Infinity };
+        return { sectionIndex: si, itemIndex: ki };
       }
     }
   }
